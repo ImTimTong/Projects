e9773f76244cd303eff6162b040d806c5d9bfad1
ImTimTong <130192910+ImTimTong@users.noreply.github.com>
2024-05-29 19:13:22 -0700

    Update pipeline_steps.groovy

--- pipeline_steps.groovy
+++ pipeline_steps.groovy
@@ -5,16 +5,35 @@ def getUserConfirmation(message) {
 def build() {
   getUserConfirmation('Proceed with build?')
   echo 'Executing Build Phase'
+  echo 'Files in current folder:'
+  sh 'ls'
 }
 
 def test() {
   getUserConfirmation('Proceed with test?')
   echo 'Executing Test Phase'
+  dir('..') {
+    echo 'Changed to folder:'
+    sh 'pwd'
+    echo 'Files in folder:'
+    sh 'ls'
+  }
+  echo 'Returned to folder:'
+  sh 'pwd'
 }
 
 def deploy() {
   getUserConfirmation('Proceed with deploy?')
   echo 'Executing Deploy Phase'
+  
+  echo 'Showing git status:'
+  sh 'git status'
+  
+  echo 'Showing environment variables:'
+  sh 'printenv'
+
+  echo 'Showing run time:'
+  sh 'uptime'
 }
 
 return this
